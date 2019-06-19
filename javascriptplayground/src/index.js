b6f7d0e34c8b6a95e4290d9ee34f38c9aa7e37cf
mbazych <44398475+mbazych@users.noreply.github.com>
2019-06-19 18:53:44 +0200

    ht

--- javascriptplayground/src/index.js
+++ javascriptplayground/src/index.js
@@ -1,12 +1,97 @@
+/*
+const square = function(number) {
+    return number * number
+}
+
+const squared = number => {
+    return number * number;
+}
+
+const squares = number => number * number
+
+
+const jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: true },
+    { id: 3, isActive: false }
+];
+
+const activeJobs = jobs.filter(job => job.isActive);
+
+
+
 const person = {
-    name: 'Mosh',
-    walk() {
-        console.log(this);
-    }
+  talk() {
+    let self = this;
+    setTimeout(() => {
+      console.log("this", this);
+    }, 1000);
+  }
 };
 
-person.walk()
+person.talk();
 
-const walk = person.walk
+const colors = ["red", "green", "blue"];
 
-walk()
+const items = colors.map(color => `<li>${color}</li>`);
+
+console.log(items)
+
+
+const address = {
+  street: '',
+  city: '',
+  country: ''
+}
+
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+const { street, city, country } = address
+
+const { street: st } = address
+
+
+const first = [1,2,3]
+
+const second = [4,5,6]
+
+// const combined = first.concat(second)
+
+// const combined = [...first, ...second]
+
+// clone first array 
+const clone = [...first]
+
+console.log(first)
+console.log(clone)
+
+
+// combine 2 objects
+const first = {
+  name: 'Mosh'
+}
+const second = {
+  job: 'Instructor'
+}
+
+const combined = {...first, ...second, location: 'Australia'}
+console.log(combined)
+
+
+const person = {
+  name: "Mosh",
+}
+
+*/
+
+// Default -> import ... from ''
+// Named -> import { ... } from ''
+import Teacher, { promote } from "./teacher";
+
+const teacher = new Teacher("Mike", "MSc");
+if (teacher != "1") {
+  console.log("LOL");
+}
+teacher.teach();
